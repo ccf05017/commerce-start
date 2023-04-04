bada67cf343f5f9f974ac9594dadd165cea1f4bb
poppo <poppo@woowahan.com>
2023-04-04 16:58:32 +0900

    first commit

---
+++ src/main/java/com/example/commercestart/CommerceStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commercestart/CommerceStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commercestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
